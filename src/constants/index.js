e2f102a97c16264561baab1d0a921bb4ef3134a8
Zia <ziakayani.official@gmail.com>
2024-05-25 04:49:08 -0700

    portfolio website

---
+++ src/constants/index.js
@@ -0,0 +1,265 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  starbucks,
+  tesla,
+  shopify,
+  carrent,
+  jobit,
+  tripguide,
+  threejs,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Web Developer",
+    icon: web,
+  },
+  {
+    title: "Shopfiy Developer",
+    icon: mobile,
+  },
+  {
+    title: "Backend Developer",
+    icon: backend,
+  },
+  {
+    title: "Digital Marketier",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+  {
+    name: "docker",
+    icon: docker,
+  },
+];
+
+const experiences = [
+  {
+    title: "Full stack developer",
+    company_name: "Fiverr",
+    icon: starbucks,
+    iconBg: "#383E56",
+    date: "March 2024 - Present",
+    points: [
+      "Design, develop, and deploy robust web applications using modern technologies like React, Node.js, Nest Js, and MongoDB",
+      "Implement responsive user interfaces with React and Redux, ensuring a seamless user experience across various devices and browsers.",
+      "Develop RESTful APIs with Node.js , Nest Js and Express, providing efficient data handling and integration with front-end applications",
+      "Desig and maintain database schemas in MongoDB and SQL databases.",
+    ],
+  },
+  {
+    title: "Backend Engineer",
+    company_name: "Programmers Force pvt ltd",
+    icon: tesla,
+    iconBg: "#E6DEDD",
+    date: "Jan 2024 - Apr 2024",
+    points: [
+      "Developed and maintained web applications backends using Nest Js, MongoDB , Postgres and other related technologies.",
+      "Collaborated with cross-functional teams including Team lead, product managers, and other developers to create high-quality products.",
+      "Implemented microservices and developeped robust backends with security and scalability features.",
+      "Participated in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "Web Developer",
+    company_name: "CodeClause",
+    icon: shopify,
+    iconBg: "#383E56",
+    date: "Oct 2023 - Dec 2023",
+    points: [
+      "Developed and maintained web applications using React.js and other related technologies.",
+      "Collaborated with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implemented responsive design and ensuring cross-browser compatibility.",
+      "Participated in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "Shopify developer",
+    company_name: "NeelumSoft",
+    icon: meta,
+    iconBg: "#E6DEDD",
+    date: "Jan 2023 - Oct 2023",
+    points: [
+      "Designed and developed custom Shopify themes , ensuring they were responsive, SEO-friendly, and aligned with client branding",
+      "Integrated various third-party Shopify apps to extend the functionality of client stores, including apps for inventory management, marketing automation, and customer support  ",
+      "Provided ongoing support and maintenance for client stores, addressing technical issues and implementing new features as needed",
+      "Conducted performance audits and optimized Shopify stores for faster load times and better user experience",
+    ],
+  },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "Zia is an exceptional  full-stack developer who deliver outstanding work, showcasing remarkable technical skills, creativity, and professionalism.",
+    name: "Gull Rehman",
+    designation: "Full Stack Developer",
+    company: "Altar Tech",
+    image: "https://scontent.fkhi10-1.fna.fbcdn.net/v/t39.30808-6/313096029_5861586140570282_8522194604129799482_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeF-5InXGxeNlFIHYPOlYaQEGatoltQ-ro8Zq2iW1D6ujysP3q2XkiUiXBdiPa_eTgSDsumVlvoYj6Bn-lW4aoIM&_nc_ohc=1p8W4GV6TykQ7kNvgFDhAtO&_nc_ht=scontent.fkhi10-1.fna&oh=00_AYCmBmxxjNG_0Pgz1_ORdGk0M7R4HBuFYZPUllwYpjHcvw&oe=6657AC64",
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Zia does.",
+    name: "David Brown",
+    designation: "SEO Expert",
+    company: "Unity Films",
+    image: "https://scontent.flhe9-1.fna.fbcdn.net/v/t39.30808-6/444485410_421753440664093_545875682842987109_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeHDEDFt0aEzglh90lPdnMUDy_9c9mcMfAjL_1z2Zwx8CBelaHebIaLh3AwG7NoMloWBjnDlrvvXsjXXBoMprtSM&_nc_ohc=xOF5xSPUi5MQ7kNvgGOKlPr&_nc_ht=scontent.flhe9-1.fna&oh=00_AYCuKQiO98E35iuZpZMcYGcyX44wq0MwLwu2qAHjP0jyLw&oe=66578A7B",
+  },
+  {
+    testimonial:
+      "Working with Zia was a revelation. His coding skills are impeccable. I strongly endorse him for any development project.",
+    name: "Aqib Awan",
+    designation: "CEO ",
+    company: "Peek Coding",
+    image: "https://scontent.fkhi10-1.fna.fbcdn.net/v/t39.30808-6/418452244_1391933024747683_8609320040231822538_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeH6bo5lPZg_nmWQdYpwVpkDV4FVlDOnRbxXgVWUM6dFvKikASWxxPJybdu6kgozD5mGKqEpJEWqzxvY2R_8QoCy&_nc_ohc=GKl_Y2gYJRwQ7kNvgGKoeuL&_nc_ht=scontent.fkhi10-1.fna&oh=00_AYCCLXB5Y_SISPCrjRMKVa7mJovVcXdtMvcnEKKE8lFFLw&oe=66576CB4",
+  },
+];
+
+const projects = [
+  {
+    name: "Bank website",
+    description:
+      "Developed a secure, robust, and user-friendly web application , providing customers with comprehensive online banking services and with modern and intuitive design.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "TailwindCSS",
+        color: "green-text-gradient",
+      },
+      {
+        name: "ShedCN",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: carrent,
+    source_code_link: "https://github.com/zia-kayani/bank-modern-app",
+  },
+  {
+    name: "LMS ",
+    description:
+      "LMS to facilitate online education and training for educational institutions. The LMS provided features for course management, user enrollment, progress tracking, assessments, and interactive learning experience",
+    tags: [
+      {
+        name: "React Js",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "Nest JS",
+        color: "green-text-gradient",
+      },
+      {
+        name: "MongoDB",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: jobit,
+    source_code_link: "https://github.com/zia-kayani/student-education-system-backend",
+  },
+  {
+    name: "Resturent App",
+    description:
+      "restaurant web application to streamline restaurant operations, enhance customer experience..application includes features for table reservations, order management, menu customization, customer feedback, and analytics.",
+    tags: [
+      {
+        name: "HTML",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "CSS",
+        color: "green-text-gradient",
+      },
+      {
+        name: "JavaScript",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: tripguide,
+    source_code_link: "https://github.com/zia-kayani/resturent-app",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
